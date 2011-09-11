3b408cf9528ac59672840e31a5d81756104ff8a2
Craig Kerstiens <craig.kerstiens@gmail.com>
2011-09-10 22:51:04 -0700

    updating to use https

--- exampleapp.py
+++ exampleapp.py
@@ -39,7 +39,7 @@ def fbapi_get_string(path, domain=u'graph', params=None, access_token=None, enco
 
 def fbapi_auth(code):
     params = {'client_id':app.config['FBAPI_APP_ID'],
-              'redirect_uri':'https://' + request.base_url,
+              'redirect_uri':'https://' + request.host,
               'client_secret':app.config['FBAPI_APP_SECRET'],
               'code':code}
     
