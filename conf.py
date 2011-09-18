7ed501bfd07d0850bea4bdb9bc6903fdb57500de
Morten Bagai <mortenbagai@gmail.com>
2011-09-18 14:29:22 -0700

    remove publish_actions requirement

--- conf.py
+++ conf.py
@@ -5,7 +5,7 @@ class Config(object):
     DEBUG = True
     TESTING = False
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
-    FBAPI_SCOPE = ['publish_actions', 'user_likes', 'user_photos',
+    FBAPI_SCOPE = ['user_likes', 'user_photos',
                    'user_photo_video_tags']
     FBAPI_APP_ID = os.environ.get('FACEBOOK_APP_ID')
     FBAPI_APP_SECRET = os.environ.get('FACEBOOK_SECRET')
